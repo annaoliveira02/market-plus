b768dc6da1058c42d8d879a64da6a964dc94525b
Gabriela <gabrielarodrigues432@gmail.com>
2021-12-06 17:39:14 -0300

    adding id in users

--- app/routes/users_blueprints.py
+++ app/routes/users_blueprints.py
@@ -7,6 +7,6 @@ bp_users = Blueprint('bp_users', __name__)
 bp_users.post("/register")(create_user)
 bp_users.post("/login")(login_user)
 bp_users.get("/users")(get_user)
-bp_users.patch("/users")(change_users)
-bp_users.delete("/users")(delete_users)
+bp_users.patch("/users/<id>")(change_users)
+bp_users.delete("/users/<id>")(delete_users)
 
