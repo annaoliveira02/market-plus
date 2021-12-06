221151b0833546abce654b008f2ef5e17addd471
KaioIwakiri <kaio_ti@hotmail.com>
2021-12-06 19:08:58 -0300

    feat: login auth, token

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,14 @@
 from flask import Flask
-from os import getenv
-from app.configs import database, migration
+from app.configs import database, migration, env_configs, jwt_auth
 from app import routes
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('SQLALCHEMY_DATABASE_URI')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JSON_SORT_KEYS'] = False
-
     database.init_app(app)
     migration.init_app(app)    
     routes.init_app(app)
+    env_configs.init_app(app)
+    jwt_auth.init_app(app)
 
     return app
