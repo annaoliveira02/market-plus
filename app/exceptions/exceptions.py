406e540bbb99fa32f22aa91a738c5e48a31a0362
Lucas Brasil <lucasmbrasil2101@gmail.com>
2021-12-07 10:45:25 -0300

    starting exceptions

---
+++ app/exceptions/exceptions.py
@@ -0,0 +1,14 @@
+class EmailAlreadyExistsError(Exception):
+    ...
+
+
+class ProductAlreadyExistsError(Exception):
+    ...
+
+
+class NotFoundError(Exception):
+    ...
+
+
+class InvalidKeyError(Exception):
+    ...
