b90626c16874b0d352f65b7c0aa29386a240cd92
Gabriela <gabrielarodrigues432@gmail.com>
2021-12-07 10:01:53 -0300

    fixing models

--- app/models/stores_models.py
+++ app/models/stores_models.py
@@ -1,22 +1,22 @@
 from dataclasses import dataclass
+
+from sqlalchemy.sql.sqltypes import LargeBinary
 from app.configs.database import db
-from sqlalchemy import Column, String, Integer, Binary
+from sqlalchemy import Column, String, Integer
 
 @dataclass
 class Stores(db.Model):
     id: int
     name: str
-    city: str
-    state: str
-    country: str
-    email: str
-    password: str
+    address: str
+    store_img: str
+    phone_number: str
 
     __tablename__ = "stores"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False, unique=True)
-    adress = Column(String(100), nullable=False)
-    store_img = Column(Binary)
+    address = Column(String(100), nullable=False)
+    store_img = Column(LargeBinary)
     phone_number = Column(String(20))
 
