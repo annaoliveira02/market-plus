5b49e989c4efb3fa606949b8560483f40924ba16
Maikol Moraes <maikolmoraesas@gmail.com>
2021-12-08 15:59:41 -0300

    feat: exceptions stores

--- app/exceptions/exceptions.py
+++ app/exceptions/exceptions.py
@@ -15,3 +15,10 @@ class NotFoundError(Exception):
 
 class NotAcessibleError(Exception):
     ...
+
+class StoreAlreadyExistsError(Exception):
+    message = {"error": "Loja já cadastrada!"}
+
+class StoreInvalidKeys(Exception):
+    allowed_keys = ["name", "adress", "store_img", "phone_number", "cnpj", "password"]
+    message = {"error": f"As chaves permitidas são: {[key for key in allowed_keys]}"}
--- app/models/stores_models.py
+++ app/models/stores_models.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
 from app.configs.database import db
 from sqlalchemy import Column, String, Integer
-from app.exceptions.exceptions import NotAcessibleError
+from app.exceptions.exceptions import NotAcessibleError, StoreAlreadyExistsError, StoreInvalidKeys
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import validates
 
 
 @dataclass
@@ -34,3 +35,18 @@ class Stores(db.Model):
 
     def validate_password(self, input_password):
         return check_password_hash(self.password_hash, input_password)
+
+    @staticmethod
+    def validate_keys(data):
+        allowed_keys = ["name", "address", "store_img", "phone_number", "cnpj", "password"]
+        for key in data:
+            if key not in allowed_keys:
+                raise StoreInvalidKeys
+
+    @validates("name", "adress", "store_img", "phone_number", "cnpj")
+    def validates(self, key, value):
+        if key == "name":
+            unique_key = Stores.query.filter(Stores.name==value).first()
+            if unique_key is not None:
+                raise StoreAlreadyExistsError
+        return value
