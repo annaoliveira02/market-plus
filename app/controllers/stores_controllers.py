59a925fa008eabd5bbbb0abbade8c6fa5f6b4666
Gabriela <gabrielarodrigues432@gmail.com>
2021-12-14 10:39:26 -0300

    query params filter by category name and page in products and store

--- app/controllers/stores_controllers.py
+++ app/controllers/stores_controllers.py
@@ -4,8 +4,7 @@ from app.exceptions.exceptions import (
     InvalidKeyError,
     InvalidTypeError,
     NotFoundError,
-    StoreAlreadyExistsError,
-    StoreInvalidKeys,
+    StoreAlreadyExistsError
 )
 from app.models.stores_models import Stores
 from flask_jwt_extended import jwt_required
@@ -23,7 +22,7 @@ def register_store():
         return e.message, 409
     except InvalidKeyError:
         return {
-            "alerta": "Informações incorretas (nome, endereço, cidade, número, cnpj e senha)."
+            "alerta": "Informações incorretas (nome, endereço, cidade, estado, número, cnpj e senha)."
         }, 400
     except InvalidTypeError:
         return {"alerta": "Informações inválidas (apenas texto)."}, 400
@@ -93,3 +92,76 @@ def login_store():
         return {"alerta": "Informações inválidas (apenas texto)."}, 400
     except NotFoundError:
         return {"alerta": "Cadastro não encontrado"}, 404
+
+
+def get_page():
+    list = Stores.query.all()
+    if request.args != {}:
+        page = int(request.args.get('page'))
+        per_page = int(request.args.get('per_page'))
+        start = (page - 1) * per_page
+        end = page * per_page
+        limited_list = list[start:end]
+    if request.args == {}:
+        limited_list = list[0:10]
+    return (
+        jsonify(
+            [
+                {
+                    "id": store.id,
+                    "name": store.name,
+                    "address": store.address,
+                    "city": store.city,
+                    "state": store.state,
+                    "phone_number": store.phone_number,
+                    "cnpj": store.cnpj,
+                    "products": [
+                        {
+                            "name": product.name,
+                            "category": product.category,
+                        }
+                        for product in store.products
+                    ],
+                }
+                for store in limited_list
+            ]
+        ),
+        200,
+    )
+
+def get_city():
+    keys = request.args.keys()
+    city = str(request.args.get('city'))
+    name = str(request.args.get('name'))
+    if request.args == {}:
+        list = Stores.query.all()
+    if "city" in keys:
+        list = Stores.query.filter(Stores.city==city).all()
+    if "name" in keys:
+        list = Stores.query.filter(Stores.name== name).all()
+    return (
+        jsonify(
+            [
+                {
+                    "id": store.id,
+                    "name": store.name,
+                    "address": store.address,
+                    "city": store.city,
+                    "state": store.state,
+                    "phone_number": store.phone_number,
+                    "cnpj": store.cnpj,
+                    "products": [
+                        {
+                            "name": product.name,
+                            "category": product.category,
+                        }
+                        for product in store.products
+                    ],
+                }
+                for store in list
+            ]
+        ),
+        200,
+    )
+
+
